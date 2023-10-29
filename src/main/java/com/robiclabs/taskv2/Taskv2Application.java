cdee034b642f0287e28ebefae4a265aa3dbba1af
Etsri Klussey <etsri.klussey@eecc.de>
2023-10-29 18:11:15 +0100

    Add init Project

---
+++ src/main/java/com/robiclabs/taskv2/Taskv2Application.java
@@ -0,0 +1,13 @@
+package com.robiclabs.taskv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taskv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taskv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/robiclabs/taskv2/Taskv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robiclabs.taskv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taskv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
