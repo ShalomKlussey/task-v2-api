8ca904badb367e568e6d21cc503da7a9d9fbd5bf
Etsri Klussey <etsri.klussey@eecc.de>
2023-11-05 12:33:10 +0100

    Report Service - delete unused property

--- src/main/java/com/robiclabs/taskv2/services/ReportService.java
+++ src/main/java/com/robiclabs/taskv2/services/ReportService.java
@@ -1,12 +1,8 @@
 package com.robiclabs.taskv2.services;
 
-import com.robiclabs.taskv2.models.Report;
 import com.robiclabs.taskv2.repositories.ReportRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ReportService {
