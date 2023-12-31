4897a903e4abc4a4582f5a0b65f758dd39c7a7dc
Etsri Klussey <etsri.klussey@eecc.de>
2023-12-31 15:31:24 +0100

    Init DB with CommandLineRunner

--- src/main/java/com/robiclabs/taskv2/Taskv2Application.java
+++ src/main/java/com/robiclabs/taskv2/Taskv2Application.java
@@ -1,7 +1,15 @@
 package com.robiclabs.taskv2;
 
+import com.robiclabs.taskv2.models.Task;
+import com.robiclabs.taskv2.repositories.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Taskv2Application {
@@ -10,4 +18,23 @@ public class Taskv2Application {
 		SpringApplication.run(Taskv2Application.class, args);
 	}
 
+	@Autowired
+	TaskRepository taskRepository;
+
+	@Bean
+	@ConditionalOnProperty(prefix = "app", name = "db.init", havingValue = "true")
+	public CommandLineRunner demoCommandLineRunner() {
+		return args -> {
+
+			System.out.println("Running.....");
+
+			Task task_1 = new Task(1L, "Report", "Description 1", Boolean.FALSE);
+			Task task_2 = new Task(2L, "Maintenance", "Description 2 again", Boolean.FALSE);
+			Task task_3 = new Task(3L, "Meeting", "Description 2", Boolean.FALSE);
+			Task task_4 = new Task(4L, "Meeting with Client", "Description ", Boolean.TRUE);
+
+			taskRepository.saveAll(List.of(task_1, task_2, task_3, task_4));
+
+		};
+	}
 }
