6618aa26ae3d33686031fb02ccac2ac106a7eb99
Etsri Klussey <etsri.klussey@eecc.de>
2023-12-31 15:34:22 +0100

    using Slf4j in Main Class

--- src/main/java/com/robiclabs/taskv2/Taskv2Application.java
+++ src/main/java/com/robiclabs/taskv2/Taskv2Application.java
@@ -2,6 +2,7 @@ package com.robiclabs.taskv2;
 
 import com.robiclabs.taskv2.models.Task;
 import com.robiclabs.taskv2.repositories.TaskRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,8 +12,10 @@ import org.springframework.context.annotation.Bean;
 
 import java.util.List;
 
+@Slf4j
 @SpringBootApplication
 public class Taskv2Application {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Taskv2Application.class, args);
@@ -26,7 +29,7 @@ public class Taskv2Application {
 	public CommandLineRunner demoCommandLineRunner() {
 		return args -> {
 
-			System.out.println("Running.....");
+			log.info("Running.....");
 
 			Task task_1 = new Task(1L, "Report", "Description 1", Boolean.FALSE);
 			Task task_2 = new Task(2L, "Maintenance", "Description 2 again", Boolean.FALSE);
@@ -35,6 +38,7 @@ public class Taskv2Application {
 
 			taskRepository.saveAll(List.of(task_1, task_2, task_3, task_4));
 
+			log.info("Init done!");
 		};
 	}
 }
